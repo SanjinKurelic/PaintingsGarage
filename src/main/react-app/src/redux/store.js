7a9c5a90e085a03e211f15043855d8f8be560f23
Sanjin Kurelić <kurelic@sanjin.eu>
2021-08-29 00:51:18 +0200

    Define redux store for images

---
+++ src/main/react-app/src/redux/store.js
@@ -0,0 +1,8 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {imageSlice} from "./imageSlice";
+
+export default configureStore({
+  reducer: {
+    imageSlice: imageSlice.reducer
+  }
+});
