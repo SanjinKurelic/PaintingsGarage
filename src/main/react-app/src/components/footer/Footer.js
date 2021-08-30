86206d8129bdd381173e04fdbdd7fb762788adcb
Sanjin Kurelić <kurelic@sanjin.eu>
2021-08-31 00:35:14 +0200

    Add footer

--- src/main/react-app/src/components/footer/Footer.js
+++ src/main/react-app/src/components/footer/Footer.js
@@ -2,9 +2,9 @@ import moment from 'moment'
 
 const Footer = () => {
   return (
-    <Footer>
+    <footer className='bg-dark text-white-50 text-center p-3'>
       <p>Copyright &copy; Sell My Picture {moment(new Date()).format('YYYY')}</p>
-    </Footer>
+    </footer>
   )
 }
 
--- src/main/react-app/src/components/header/Header.js
+++ src/main/react-app/src/components/header/Header.js
@@ -1,22 +1,24 @@
 import logo from '../../assets/logo.png'
-import {Button, Form, FormControl, Image, InputGroup, Nav} from 'react-bootstrap'
+import {Button, Container, Form, FormControl, Image, InputGroup, Nav} from 'react-bootstrap'
 import {BsSearch} from 'react-icons/bs'
 import {FaShoppingCart, FaUser} from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <Nav className="d-flex justify-content-between p-2">
-      <Image src={logo} height={40}/>
-      <Form className="mx-3 flex-grow-1" style={{maxWidth: '600px'}}>
-        <InputGroup>
-          <FormControl/>
-          <InputGroup.Text><BsSearch/></InputGroup.Text>
-        </InputGroup>
-      </Form>
-      <div>
-        <Button variant="link" size="lg"><FaUser size={28} color="black"/></Button>
-        <Button variant="link" size="lg"><FaShoppingCart size={28} color="black"/></Button>
-      </div>
+    <Nav className="bg-light">
+      <Container className="d-flex justify-content-between p-2">
+        <Image className='my-2' src={logo} height={40}/>
+        <Form className="mx-3 my-2 flex-grow-1" style={{maxWidth: '600px'}}>
+          <InputGroup>
+            <FormControl/>
+            <InputGroup.Text><BsSearch/></InputGroup.Text>
+          </InputGroup>
+        </Form>
+        <div>
+          <Button variant="link" size="lg"><FaUser size={28} color="black"/></Button>
+          <Button variant="link" size="lg"><FaShoppingCart size={28} color="black"/></Button>
+        </div>
+      </Container>
     </Nav>
   )
 }
