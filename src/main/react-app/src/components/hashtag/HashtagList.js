8b801ddf50769a32a8f1d3b32ab30345386db87e
Sanjin Kurelić <kurelic@sanjin.eu>
2021-08-31 00:47:19 +0200

    Use hashtag element instead of custom one

--- src/main/react-app/src/components/hashtag/HashtagList.js
+++ src/main/react-app/src/components/hashtag/HashtagList.js
@@ -1,22 +1,45 @@
 import PropTypes from 'prop-types'
-import Hashtag from './Hashtag'
+import {WithContext as ReactTag} from 'react-tag-input'
+import './hashtagList.scss'
 
-const HashtagList = ({hashtagItems}) => {
+// Define styles
+const styleClasses = {
+  tag: 'hashtag-tag'
+}
+
+const HashtagList = ({hashtagItems, readOnly}) => {
+  // Format data
+  const formatTags = () => {
+    if (readOnly === false) {
+      return hashtagItems;
+    }
+
+    return hashtagItems.map((tag, index) => {
+      return {
+        id: ('' + index),
+        text: ('#' + tag)
+      }
+    })
+  }
+  const tags = formatTags();
+
+  const tagClick = (index) => {
+    console.log(tags[index]); // TODO search by tag
+  }
+
   return (
-    <>
-      {hashtagItems.map((hashtag, index) => (
-        <Hashtag key={index} value={hashtag}/>
-      ))}
-    </>
+    <ReactTag classNames={styleClasses} tags={tags} readOnly={readOnly} handleTagClick={tagClick}/>
   )
 }
 
 HashtagList.propTypes = {
-  hashtagItems: PropTypes.array
+  hashtagItems: PropTypes.array,
+  readOnly: PropTypes.bool
 }
 
 HashtagList.defaultProps = {
-  hashtagItems: []
+  hashtagItems: [],
+  readOnly: true
 }
 
 export default HashtagList
