745bccb6b649a6428bf621fff58e413703ef0c05
Sanjin Kurelic <skurelic@croz.net>
2021-09-11 21:02:08 +0200

    Stick footer to bottom

--- src/main/react-app/src/components/footer/Footer.js
+++ src/main/react-app/src/components/footer/Footer.js
@@ -2,8 +2,8 @@ import moment from 'moment'
 
 const Footer = () => {
   return (
-    <footer className='bg-dark text-white-50 text-center p-3'>
-      <p>Copyright &copy; Sell My Picture {moment(new Date()).format('YYYY')}</p>
+    <footer className='bg-dark text-white-50 text-center p-3 position-fixed bottom-0 w-100 overflow-hidden'>
+      <p className='mb-0'>Copyright &copy; Sell My Picture {moment(new Date()).format('YYYY')}</p>
     </footer>
   )
 }
