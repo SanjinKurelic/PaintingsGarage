4783fd4785cd16dff6d2729051df54424f85335c
Sanjin Kurelic <skurelic@croz.net>
2021-09-11 21:02:01 +0200

    Define browser router

--- src/main/react-app/src/App.js
+++ src/main/react-app/src/App.js
@@ -5,20 +5,27 @@ import {useState} from 'react'
 import Image from './components/image/Image'
 import {useGetLatestImagesQuery} from './redux/api/searchApi'
 import Footer from './components/footer/Footer'
+import {BrowserRouter, Route} from 'react-router-dom'
+import Login from './components/login/Login'
 
 function App() {
   const latestImages = useGetLatestImagesQuery()
   const [selectedImage, setSelectedImage] = useState(null)
 
   return (
-    <>
+    <BrowserRouter>
       <Header/>
       <Container>
-        {latestImages.isSuccess && <ImageGallery images={latestImages.data} setSelectedImage={setSelectedImage}/>}
-        {selectedImage && <Image image={selectedImage} setSelectedImage={setSelectedImage}/>}
+        <Route path="/" exact render={() => (
+          <>
+            {latestImages.isSuccess && <ImageGallery images={latestImages.data} setSelectedImage={setSelectedImage}/>}
+            {selectedImage && <Image image={selectedImage} setSelectedImage={setSelectedImage}/>}
+          </>
+        )}/>
+        <Route path="/login" component={Login}/>
       </Container>
       <Footer/>
-    </>
+    </BrowserRouter>
   )
 }
 
