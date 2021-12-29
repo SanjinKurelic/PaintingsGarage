de3f6f92631cef5d94734920f2b0edce78ab5bc2
Sanjin Kurelić <kurelic@sanjin.eu>
2021-12-29 13:16:06 +0100

    Move user related logic to separate package

---
+++ src/main/java/eu/sanjin/kurelic/photostorage/modules/user/entity/UserRole.java
@@ -0,0 +1,13 @@
+package eu.sanjin.kurelic.photostorage.modules.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum UserRole {
+  ROLE_ADMIN("ADMIN"), ROLE_USER("USER");
+
+  @Getter
+  private final String applicationRole;
+}
