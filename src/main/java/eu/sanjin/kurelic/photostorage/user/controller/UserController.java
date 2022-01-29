9c6553c28e90a1f7cc10cecb96079f0023805dcc
Sanjin Kurelic <skurelic@croz.net>
2022-01-29 14:06:42 +0100

    Define admin backend for user interaction

--- src/main/java/eu/sanjin/kurelic/photostorage/user/controller/UserController.java
+++ src/main/java/eu/sanjin/kurelic/photostorage/user/controller/UserController.java
@@ -1,12 +1,17 @@
 package eu.sanjin.kurelic.photostorage.user.controller;
 
 import eu.sanjin.kurelic.photostorage.common.model.SearchResult;
+import eu.sanjin.kurelic.photostorage.security.model.UserDetailsModel;
 import eu.sanjin.kurelic.photostorage.user.model.Author;
+import eu.sanjin.kurelic.photostorage.user.model.UserPlanRequest;
 import eu.sanjin.kurelic.photostorage.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,6 +38,22 @@ public class UserController {
   @GetMapping("/list")
   public ResponseEntity<List<Author>> getAuthorList() {
     return ResponseEntity.ok(service.getAuthors());
+  }
+
+  @PostMapping("/changePlan")
+  public void changePlan(@RequestBody UserPlanRequest plan) {
+    var principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    if (Objects.nonNull(plan.plan()) && Objects.nonNull(principal) && principal instanceof UserDetailsModel user) {
+      service.changePlan(user.getId(), plan.plan());
+    }
+  }
+
+  @PostMapping("/changePlanForUser/{:userId}")
+  public void changePlanForUser(@PathVariable Long userId, @RequestBody UserPlanRequest plan) {
+    if (Objects.nonNull(plan.plan())) {
+      service.changePlan(userId, plan.plan());
+    }
   }
 
   @GetMapping("/find/{authorName}")
---
+++ src/main/java/eu/sanjin/kurelic/photostorage/user/model/UserPlanRequest.java
@@ -0,0 +1,6 @@
+package eu.sanjin.kurelic.photostorage.user.model;
+
+import eu.sanjin.kurelic.photostorage.user.entity.UserPlan;
+
+public record UserPlanRequest(UserPlan plan) {
+}
