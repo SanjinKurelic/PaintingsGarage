e3123dadd62e87ea79ce25e3efb57a89cced8508
Sanjin Kurelic <skurelic@croz.net>
2022-01-30 02:10:09 +0100

    Add persist store to frontend

--- src/main/react-app/src/index.js
+++ src/main/react-app/src/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
-import {store} from './redux/store'
+import {store, persistor} from './redux/store'
 import {Provider} from 'react-redux'
+import {PersistGate} from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <PersistGate persistor={persistor}>
+        <App/>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/main/react-app/src/redux/store.js
+++ src/main/react-app/src/redux/store.js
@@ -1,4 +1,4 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import {photoApi} from './api/photoApi'
 import {setupListeners} from '@reduxjs/toolkit/query'
 import {userApi} from './api/userApi'
@@ -6,19 +6,31 @@ import {hashtagApi} from './api/hashtagApi'
 import {authApi} from './api/authApi'
 import currentUserSlice from './slice/currentUserSlice'
 import {auditApi} from './api/auditApi'
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['currentUser']
+}
+
+const reducers = combineReducers({
+  [hashtagApi.reducerPath]: hashtagApi.reducer,
+  [photoApi.reducerPath]: photoApi.reducer,
+  [userApi.reducerPath]: userApi.reducer,
+  [authApi.reducerPath]: authApi.reducer,
+  [auditApi.reducerPath]: auditApi.reducer,
+  currentUser: currentUserSlice
+})
 
 export const store = configureStore({
-  reducer: {
-    [hashtagApi.reducerPath]: hashtagApi.reducer,
-    [photoApi.reducerPath]: photoApi.reducer,
-    [userApi.reducerPath]: userApi.reducer,
-    [authApi.reducerPath]: authApi.reducer,
-    [auditApi.reducerPath]: auditApi.reducer,
-    currentUser: currentUserSlice
-  },
+  reducer: persistReducer(persistConfig, reducers),
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
     photoApi.middleware, userApi.middleware, hashtagApi.middleware, authApi.middleware, auditApi.middleware
   )
 })
+
+export const persistor = persistStore(store)
 
 setupListeners(store.dispatch)
