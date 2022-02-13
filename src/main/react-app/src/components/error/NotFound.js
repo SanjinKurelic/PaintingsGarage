56d268d27466fa6996e3a94c96d2beb6aa15689f
Sanjin Kurelic <skurelic@croz.net>
2022-02-13 20:57:11 +0100

    Implement 404 frontend

--- src/main/react-app/src/components/error/NotFound.js
+++ src/main/react-app/src/components/error/NotFound.js
@@ -1,5 +1,21 @@
+import error404 from '../../assets/404.png'
+import {Button} from 'react-bootstrap'
+import {useHistory} from 'react-router-dom'
+
 const NotFound = () => {
-  return (<p>Not found</p>)
+  const history = useHistory()
+
+  return (
+    <>
+      <div className="text-center m-5"><img alt="" src={error404}/></div>
+      <div className="m-3 fst-italic alert alert-warning">
+        Requested page was not found, please go to home screen by clicking this link:&nbsp;
+        <Button variant="link" className="fst-italic p-0 align-baseline" onClick={() => {
+          history.push('/')
+        }}>Home page</Button>
+      </div>
+    </>
+  )
 }
 
 export default NotFound
