f0c8cee70920040babd77970e1a8c0289201cd04
Sanjin Kurelic <skurelic@croz.net>
2022-04-11 17:40:13 +0200

    Initial photo test cases

---
+++ src/test/java/eu/sanjin/kurelic/paintingsgarage/photo/controller/PhotoControllerIntegrationTest.java
@@ -0,0 +1,44 @@
+package eu.sanjin.kurelic.paintingsgarage.photo.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import eu.sanjin.kurelic.paintingsgarage.photo.model.PhotoData;
+import eu.sanjin.kurelic.paintingsgarage.testutil.type.IntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@IntegrationTest
+class PhotoControllerIntegrationTest {
+
+  @Autowired
+  protected ObjectMapper mapper;
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  void getPhotoList() throws Exception {
+    // Given
+    var request = MockMvcRequestBuilders
+      .request(HttpMethod.GET, "/api/photo")
+      .contentType(MediaType.APPLICATION_JSON);
+
+    // When
+    var result = mvc.perform(request).andDo(MockMvcResultHandlers.print()).andReturn();
+
+    var response = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<List<PhotoData>>() {
+    });
+
+    // Then
+    assertThat(response).hasSize(1);
+  }
+}
